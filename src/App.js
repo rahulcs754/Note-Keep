977e7f8a79e9cb2f40d85d43397c3a4f4840a91e
rahulcs754 <rahul.shukla754@gmail.com>
2022-04-13 23:52:36 +0530

    Initial repo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Routespaths from "./fronted/Router";
+function App() {
+  return (
+    <div className="App">
+      <Routespaths />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fronted/Pages/index.js
@@ -0,0 +1,3 @@
+export { Homepage } from "./Homepage";
+export { Login } from "./Login";
+export { Register } from "./Register";
